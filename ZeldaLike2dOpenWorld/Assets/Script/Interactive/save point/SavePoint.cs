753a0ee74b680eaa1b94be5a4309816addfa0fc6
Nahyox <lazare@ilico.org>
2024-10-01 22:27:59 +0200

    add interactiv object

---
+++ ZeldaLike2dOpenWorld/Assets/Script/Interactive/save point/SavePoint.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavePoint : MonoBehaviour
+{
+
+    public bool isInRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(isInRange==true)
+        {
+            if(Input.GetKeyDown("e"))
+            {
+                Debug.Log("you saved your game");
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("IN RANGE");
+        if (collision.CompareTag("Player"))
+        {
+            isInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isInRange = false;
+        }
+    }
+}
