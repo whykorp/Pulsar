727f30ad7786647db61dc37ea0d54a20c9c857cf
Nahyox <lazare@ilico.org>
2024-10-02 17:24:09 +0200

    update

--- ZeldaLike2dOpenWorld/Assets/Script/Ui/ShowUi.cs
+++ ZeldaLike2dOpenWorld/Assets/Script/Ui/ShowUi.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,17 +8,31 @@ using UnityEngine.UI;
 
 public class ShowUi : MonoBehaviour
 {
-    //NEED add the link library for text
-    public Text texte;
+    
+    public bool test=false;
+    public TextMeshProUGUI texte;
     // Start is called before the first frame update
     void Start()
     {
-        
+        texte.enabled=false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            if(test==true)
+            {
+                texte.enabled = true;
+                test = false;
+            }
+            else
+            {
+                texte.enabled = false;
+                test = true;
+            }
+        }
         
     }
 }
