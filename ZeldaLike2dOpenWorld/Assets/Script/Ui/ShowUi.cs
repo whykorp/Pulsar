31d842da4ba887a269d65a4b1cb9a55597ab5a7b
Nahyox <lazare@ilico.org>
2024-10-02 14:07:42 +0200

    patch bugs

--- ZeldaLike2dOpenWorld/Assets/Script/Ui/ShowUi.cs
+++ ZeldaLike2dOpenWorld/Assets/Script/Ui/ShowUi.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 
 public class ShowUi : MonoBehaviour
 {
     //NEED add the link library for text
-    public text Texte;
+    public Text texte;
     // Start is called before the first frame update
     void Start()
     {
