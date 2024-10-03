173fa8ed025457890a6b9319d8bda0cc51be6d61
Nahyox <lazare@ilico.org>
2024-10-03 09:36:24 +0200

    Add Enemy detection

--- ZeldaLike2dOpenWorld/Assets/Script/Mobs/Hostile/PlayerDetection.cs
+++ ZeldaLike2dOpenWorld/Assets/Script/Mobs/Hostile/PlayerDetection.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerDetection : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public EnemyMovement enemyMovement;
+    public Transform playerTransform;
+    private bool isDetected=false;
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.CompareTag("Player"))
+        {
+            if(isDetected==false)
+            {
+                isDetected=true;
+                for(int i=0;i <= enemyMovement.waypoints.Length;i++)
+                {
+                    enemyMovement.waypoints[i]=playerTransform;
+                    Debug.Log("tour de boucle:"+i);
+                }
+            }
+        } 
     }
 }
