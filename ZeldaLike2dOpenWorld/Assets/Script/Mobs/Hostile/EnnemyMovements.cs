a305fd32b9f99611e27b365081e006dd7ee90df4
Whykioh_Lycée <whykioh@gmail.com>
2024-10-03 08:31:10 +0200

    Add Ennemy

---
+++ ZeldaLike2dOpenWorld/Assets/Script/Mobs/Hostile/EnnemyMovements.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovements : MonoBehaviour
+{
+    public float speed;
+    public Transform[] waypoints;
+
+    public int damageOnCollision = 20;
+
+    public SpriteRenderer graphics;
+    private Transform target;
+    private int destPoint = 0;
+
+    void Start()
+    {
+        target = waypoints[0];
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        // Si l'ennemi est quasiment arrivé à sa destination
+        if(Vector3.Distance(transform.position, target.position) < 0.3f)
+        {
+            destPoint = (destPoint + 1) % waypoints.Length;
+            target = waypoints[destPoint];
+            graphics.flipX = !graphics.flipX;
+        }
+    }
+}
