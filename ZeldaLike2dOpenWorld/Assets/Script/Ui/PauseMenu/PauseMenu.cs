5625dee10e2e34777de8ec9ee2a7314f46b417e8
WhykiohHome <whykioh@gmail.com>
2024-10-21 15:48:41 +0200

    Import Kriss Texture

--- ZeldaLike2dOpenWorld/Assets/Script/Ui/PauseMenu/PauseMenu.cs
+++ ZeldaLike2dOpenWorld/Assets/Script/Ui/PauseMenu/PauseMenu.cs
@@ -13,19 +13,23 @@ public class PauseMenu : MonoBehaviour
     public string mainMenu = "MainMenu";
     public void GameStart(){
         pauseMenu.SetActive(false);
+        Debug.Log("OH OH POUTON PRESSE");
     }
 
     public void SettingsButton(){
         settingsWindow.SetActive(true);
         mainWindow.SetActive(false);
+        Debug.Log("OH OH POUTON PRESSE");
     }
 
     public void BackSettingsButton(){
         settingsWindow.SetActive(false);
         mainWindow.SetActive(true);
+        Debug.Log("OH OH POUTON PRESSE");
     }
 
     public void QuitGame(){
         SceneManager.LoadScene(mainMenu);
+        Debug.Log("OH OH POUTON PRESSE");
     }
 }
