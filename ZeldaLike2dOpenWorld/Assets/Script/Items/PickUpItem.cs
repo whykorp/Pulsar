74641a6a99a318d5746f0bae99608b1f4e7291c8
WhykiohHome <whykioh@gmail.com>
2024-11-14 09:53:25 +0100

    PickUpItem System

---
+++ ZeldaLike2dOpenWorld/Assets/Script/Items/PickUpItem.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUpItem : MonoBehaviour
+{
+    private bool isInRange;
+    public InteractUI interactUI;
+    public Item item;
+    // public AudioClip soundToPlay;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && isInRange)
+        {
+            TakeItem();
+        }
+    }
+
+    void TakeItem()
+    {
+        Inventory.instance.content.Add(item);
+        Inventory.instance.UpdateInventoryUI();
+        // AudioManager.instance.PlayClipAt(soundToPlay, transform.position);
+        interactUI.HideUiInteract();
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            Debug.Log("IN RANGE");
+            interactUI.ShowUiInteract();
+            isInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("OUT RANGE");
+            interactUI.HideUiInteract();
+            isInRange = false;
+        }
+    }
+}
